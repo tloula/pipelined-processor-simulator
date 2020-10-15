282c5935ce84ebe30dbbcff755c6a7a53ecb1de8
Trevor Loula <trevorloula@outlook.com>
2020-10-14 22:26:02 -0400

    Updated Assembly Files

---
+++ simple_sum.s
@@ -0,0 +1,31 @@
+Begin Assembly
+ADDI R8, R0, 2000
+ADDI R1, R0, 10
+ADDI R2, R0, 9
+ADDI R3, R0, 8
+ADDI R4, R0, 7
+ADDI R5, R0, 6
+ADDI R1, R1, -1
+ADDI R6, R0, 5
+SUB R7, R2, R3
+SW R1, 2000(R8)
+HALT
+End Assembly
+Begin Data 4000 64
+25
+0
+0
+0
+0
+0
+0
+0
+0
+0
+0
+0
+0
+0
+0
+0
+End Data
